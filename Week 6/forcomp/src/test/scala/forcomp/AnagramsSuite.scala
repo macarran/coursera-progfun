e1de818cb78b36316879f60a9414bbba831911ff
Carranza, Marcos A <marcos.a.carranza@intel.com>
2014-06-03 20:56:24 -0700

    implementing wordOccurrences and sentenceOccurrences

--- Week 6/forcomp/src/test/scala/forcomp/AnagramsSuite.scala
+++ Week 6/forcomp/src/test/scala/forcomp/AnagramsSuite.scala
@@ -24,6 +24,9 @@ class AnagramsSuite extends FunSuite {
     assert(sentenceOccurrences(List("abcd", "e")) === List(('a', 1), ('b', 1), ('c', 1), ('d', 1), ('e', 1)))
   }
 
+  test("sentenceOccurrences: abcd eab bc") {
+    assert(sentenceOccurrences(List("abcd", "eab", "bc")) === List(('a', 2), ('b', 3), ('c', 2), ('d', 1), ('e', 1)))
+  }
 
 
   test("dictionaryByOccurrences.get: eat") {
